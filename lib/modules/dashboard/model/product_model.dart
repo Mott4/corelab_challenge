bce442e3aa9def33e12531c72f3ea13f89c3a881
Vitor Mota <93683859+Mott4@users.noreply.github.com>
2024-08-24 18:26:57 -0300

    Update product_model.dart

--- lib/modules/dashboard/model/product_model.dart
+++ lib/modules/dashboard/model/product_model.dart
@@ -1,17 +1,3 @@
-// pensando na regra de negocio, nesse modulo teremos
-// titulo do item - Condicionador Ácido Fosfórico 37% - AllPrime
-// valor do item - R$29,90
-// descricao do pagamento do item - Em até 12x de R$ 249,00
-// estado do item - ex, novo, usado, seminovo
-// imagem do item
-
-// para os itens com promocao
-// porcentagem do desconto
-// valor antigo
-// valor novo
-
-// todos os itens estarão numa categoria
-
 class ProductModel {
   final String imagePath;
   final String title;
