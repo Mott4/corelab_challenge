6d9070bc70776e9e0bdfe7957e36b1f173c0c4c3
Mott4 <vitorm.lim4@gmail.com>
2024-08-24 18:13:16 -0300

    feat: app_module, app and main files have been configured

--- lib/app.dart
+++ lib/app.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Corelab Challenge',
       theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple), useMaterial3: true),
       routerConfig: Modular.routerConfig,
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:corelab_challenge/app.dart';
 import 'package:corelab_challenge/modules/app_module.dart';
+import 'package:corelab_challenge/modules/dashboard/view_model/dashboard_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(ModularApp(
-    module: AppModule(),
-    child: const MyApp(),
-  ));
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => DashboardController(),
+        ),
+      ],
+      child: ModularApp(
+        module: AppModule(),
+        child: const MyApp(),
+      ),
+    ),
+  );
 }
