ea4521e2b5aa24c8577869b600700fc5e99b8306
Mott4 <vitorm.lim4@gmail.com>
2024-08-26 08:35:48 -0300

    feat: developing branch + search module and view

---
+++ lib/modules/search/view/seach_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SearchView extends StatelessWidget {
+  const SearchView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
