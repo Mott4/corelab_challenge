e6def7a6ee40d11bc0c8100ddb60ed8e2723dbfb
Mott4 <vitorm.lim4@gmail.com>
2024-08-29 13:11:15 -0300

    feat: search module has been created - mock for search products; - view and view_model created and configured; - historic_Tile to search history

--- lib/modules/search/search_module.dart
+++ lib/modules/search/search_module.dart
@@ -1,10 +1,26 @@
+import 'package:corelab_challenge/core/services/repositories/search_repository.dart';
+import 'package:corelab_challenge/modules/search/view_model/search_view_model.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-import 'view/seach_view.dart';
+import 'view/search_view.dart';
 
 class SearchModule extends Module {
+  void _repository(Injector i) {
+    i.add<ISearchRepository>(SearchRepository.new);
+  }
+
+  void _controller(Injector i) {
+    i.addLazySingleton(SearchViewModel.new);
+  }
+
+  @override
+  void binds(Injector i) {
+    _controller(i);
+    _repository(i);
+  }
+
   @override
   void routes(RouteManager r) {
-    r.child('/', child: (context) => const SearchView());
+    r.child('/', child: (context) => SearchView(controller: Modular.get()));
   }
 }
